7dd08a46f0bc26b718b39eaaec7fecbcb5798130
UkashaAhmed <136858494+UkashaAhmed@users.noreply.github.com>
2025-12-03 17:51:26 -0500

    Add comments to explain reverse linked list logic
    Added detailed comments explaining the reverse linked list algorithm.

--- ReversedLinkedList.java
+++ ReversedLinkedList.java
@@ -13,3 +13,21 @@ class Solution {
         return prev; // new head
     }
 }
+
+
+/**
+ * To reverse a linked list, I use three pointers: prev, current, and next.
+ * 
+ * I start with:
+ *   prev = null
+ *   current = head
+ *
+ * While current is not null:
+ *   1. Save the next node: next = current.next
+ *   2. Reverse the pointer: current.next = prev
+ *   3. Move prev forward: prev = current
+ *   4. Move current forward: current = next
+ *
+ * When the loop ends, prev will be pointing to the new head of the reversed list,
+ * so I return prev.
+ */
